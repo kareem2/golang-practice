33008b4dbf498778051c8cbd8853c7fa54a87600
karim <k.harazeen@gmail.com>
2020-02-26 02:27:50 +0200

    exercise-1-9

---
+++ ch1/exercise-1-9.go
@@ -0,0 +1,34 @@
+// fetch prints the content found at a URL
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if strings.HasPrefix(url, "http://") == false {
+			if strings.HasPrefix(url, "https://") == false {
+				url = "http://" + url
+			}
+		}
+		fmt.Println(url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		responseStatus := resp.Status
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("Response status: %s\n%s\n", responseStatus, b)
+	}
+}
